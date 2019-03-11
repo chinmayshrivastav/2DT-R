95cf812511a790314293d323671dd257cf179327
chinmayshrivastav <36117071+chinmayshrivastav@users.noreply.github.com>
2019-03-11 03:55:20 -0700

    Add files via upload

---
+++ initial_commits/CMakeFiles/RectangleDetector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RectangleDetector.dir/RectangleDetector.cpp.o"
+  "RectangleDetector.pdb"
+  "RectangleDetector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RectangleDetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ initial_commits/rectdetect/CMakeFiles/rectdetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rectdetect.dir/rectdetect.cpp.o"
+  "rectdetect.pdb"
+  "rectdetect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rectdetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
